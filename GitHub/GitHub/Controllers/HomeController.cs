c08be56d022e4c875f0aa400286cd4e4a08b73a5
Mounica <525mounica@gmail.com>
2020-04-17 20:01:09 +0530

    Simple Client details display

---
+++ GitHub/GitHub/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GitHub.Models;
+
+namespace GitHub.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Clients obj = new Clients();
+            obj.ClientId = 1001;
+            obj.ClientName = "Client1";
+            return View("ClientsView",obj);
+        }
+    }
+}
---
+++ GitHub/GitHub/Models/Clients.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GitHub.Models
+{
+    public class Clients
+    {
+        public int ClientId { get; set; }
+        public string ClientName { get; set; }
+    }
+}
